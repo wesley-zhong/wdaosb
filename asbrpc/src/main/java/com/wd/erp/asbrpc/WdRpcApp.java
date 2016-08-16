38c15c9381e5af4b5d0f362c2e60bcfc679dd5db
laughing-zhong <wiqi.zhong@gmail.com>
2016-08-16 18:22:40 +0800

    update

--- asbrpc/src/main/java/com/wd/erp/asbrpc/WdRpcApp.java
+++ asbrpc/src/main/java/com/wd/erp/asbrpc/WdRpcApp.java
@@ -5,8 +5,6 @@ import org.apache.xbean.spring.context.FileSystemXmlApplicationContext;
 import org.springframework.context.ApplicationContext;
 
 import com.wd.erp.asbrpc.service.WdRpcService;
-import com.wd.erp.asbrpc.utils.AsbEncode;
-import com.wd.erp.asbrpc.utils.TimeUtil;
 
 /**
  *  wd rpc main app
@@ -18,14 +16,9 @@ public class WdRpcApp
     private static ApplicationContext ac;
 
 	public static void main( String[] args ) throws Exception
-    {
-		
-		String strNow = TimeUtil.getNowDate();
-		System.out.println(strNow + " base encdoe = " + AsbEncode.urlEncode(strNow));
-		
-		
-//    	ac = new FileSystemXmlApplicationContext(new String[]{"src/main/applicationContext.xml","src/main/sqlserver-dal-context.xml"});
-//    	WdRpcService wdRpcService = (WdRpcService)ac.getBean(WdRpcService.class);
-//    	wdRpcService.sendRpcData();
+    {	
+    	ac = new FileSystemXmlApplicationContext(new String[]{"src/main/applicationContext.xml","src/main/sqlserver-dal-context.xml"});
+    	WdRpcService wdRpcService = (WdRpcService)ac.getBean(WdRpcService.class);
+    	wdRpcService.sendRpcData();
     }
 }
